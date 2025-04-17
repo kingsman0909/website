9418033f98015bb21eb57971e808a0b003727584
kingsman0909 <75459928+kingsman0909@users.noreply.github.com>
2025-04-17 17:53:49 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,23 +1,5 @@
 <?php
-$conn=mysqli_connect("localhost", "root","","project");
-if($conn){
-    echo "Connected";
-}
-else{
-    echo "Failed";
-}
+echo "hahahaha";
 
-$username=$_POST['username'];
-$password=$_POST['password'];
-
-$data = "INSERT INTO login VALUE ('', '$username', '$password') ";
-$check = mysqli_query($conn, $data);
-
-if($check){
-    echo "Data sended";
-}
-else{
-    echo "Data couldnt send";
-}
 
 ?>
